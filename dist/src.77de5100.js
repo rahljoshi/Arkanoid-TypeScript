cb67035a994ef289997c82d30f52cbb96ed5fe4e
rahljoshi <mrjoshi0147@gmail.com>
2021-02-26 13:23:17 +0530

    code

--- dist/src.77de5100.js
+++ dist/src.77de5100.js
@@ -721,7 +721,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59110" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62256" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
